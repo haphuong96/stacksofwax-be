245a8e6b587281f9ba03fc50b91daf53b1ddb599
Phedra <haphuong96@gmail.com>
2023-04-14 22:05:45 +0700

    fix get decades filter logic

--- src/services/decade.service.js
+++ src/services/decade.service.js
@@ -1,7 +1,7 @@
 const db = require('../utils/db-execution.util');
 
 async function findAllValidDecades() {
-    const query = "SELECT DISTINCT release_year FROM album ORDER BY release_year DESC";
+    const query = "SELECT TRUNCATE(release_year, -1) as decade FROM album GROUP BY decade DESC;";
     const data = await db.execute(query);
     return data;
 }
